fdab2ca697515c06283f2017a7f89b5aa94b79ce
MahinAshraful <150500453+MahinAshraful@users.noreply.github.com>
2024-11-28 21:59:36 -0500

    script

---
+++ run.js
@@ -0,0 +1,16 @@
+const { exec } = require("child_process");
+
+
+// Open a new terminal for the React client
+exec(`start cmd /k "cd client && npm run dev"`, (err) => {
+  if (err) console.error("Error starting client:", err.message);
+});
+
+
+// Open a new terminal for the Python server
+exec(`start cmd /k "cd server && python server2.py"`, (err) => {
+  if (err) console.error("Error starting server:", err.message);
+});
+
+
+
